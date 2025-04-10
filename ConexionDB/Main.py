a34fc3fe6f8c51a97bc7d907614657921e5092af
David Lopez <davidlopezing.1@gmail.com>
2025-04-09 21:23:03 -0500

    API actualizada para conectarse a la basde de datos

--- ConexionDB/Main.py
+++ ConexionDB/Main.py
@@ -6,27 +6,33 @@ from pydantic import BaseModel
 
 
 app = FastAPI()
-class DB_manager():
-    def __init__(self) -> None:
-        db = {}
-
-    def get(SELF, ID )
-
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+    
+class DB_manager():
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    def __init__(self) -> None:
+        self.db = {}
+
+    def get(self, id: int):
+        return self.db.get(id)
+    
+    def insert(self, id: int, item: Item):
+        self.db[id] = item
+
+
+db_manager = DB_manager()
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return db_manager.get(item_id)
 
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
+
+    db_manager.insert(item_id, item)
     return {"item_name": item.name, "item_id": item_id}
